9d7b3728fd3c6870f3c14e9cd7e4c8c0b6b7121c
Yuan <49439486+Sky-And-Ground@users.noreply.github.com>
2025-11-21 10:41:41 +0800

    Update parse.cpp

--- parse.cpp
+++ parse.cpp
@@ -1,31 +1,23 @@
+/*
+    http request parser, only for header part.
+    written in C++17.
+*/
+#include <iostream>
+#include <vector>
 #include <string>
 #include <map>
-#include <iostream>
-#include <ranges>
-#include <expected>
-#include <utility>
-#include <cstdint>
 
-using namespace std::string_literals;
-using namespace std::ranges;
-
-constexpr uint32_t METHOD_MAX_LEN = 32;
-constexpr uint32_t URL_MAX_LEN = 1024;
-constexpr uint32_t VERSION_MAX_LEN = 32;
-
-enum class ParseError {
+enum class ParseResult {
+    success,
     method_too_long,
     url_too_long,
     version_too_long,
+    invalid_method,
+    invalid_url,
+    invalid_version,
+    invalid_format,
+    invalid_headers,
     invalid_crlf
-};
-
-enum class ParseState {
-    method,
-    url,
-    version,
-    headers,
-    body
 };
 
 struct Request {
@@ -33,212 +25,230 @@ struct Request {
     std::string url;
     std::string version;
     std::map<std::string, std::string> headers;
-    std::string body;
 };
 
-const char* get_parse_error_str(ParseError e) noexcept {
-    switch(e) {
-        case ParseError::method_too_long:
-            return "method too long";
-        case ParseError::url_too_long:
-            return "url too long";
-        case ParseError::version_too_long:
-            return "version too long";
-        case ParseError::invalid_crlf:
-            return "invalid crlf";
-        default:
-            return "error unknown";
-    }
-}
+class Parser {
+    static constexpr char SPACE = ' ';
+    static constexpr char CR = '\r';
+    static constexpr char LF = '\n';
+    static constexpr const char* CRLF = "\r\n";
 
-/**
- * if parse success, return a uint8_t value,
- * else return an error message.
-*/
-std::expected<uint8_t, std::string> hex_to_decimal(char c) noexcept {
-    if (c >= '0' && c <= '9'){
-        return c - '0';
-    }
-    else if (c >= 'a' && c <= 'f'){
-        return c - 'a' + 10;
-    }
-    else if (c >= 'A' && c <= 'F'){
-        return c - 'A' + 10;
-    }
-    else {
-        return std::unexpected("param should be 0 ~ 9, a ~ f or A ~ F");
-    }
-}
+    enum class State {
+        method,
+        url,
+        version,
+        headers
+    };
 
-/**
- * if parse success, return the decoded url string,
- * else return an error message.
-*/
-std::expected<std::string, std::string> percent_encoding_url_decode(std::string const& url) {
-    std::string ret;
-    auto urlLen = url.length();
-
-    for (auto i = 0zu; i < urlLen;) {
-        if (url[i] == '%') {
-            if (i + 2 >= urlLen) {
-                return std::unexpected("invalid percent encoding url");
-            }
-
-            auto p1 = hex_to_decimal(url[i + 1]);
-            auto p2 = hex_to_decimal(url[i + 2]);
-
-            if (p1 && p2) {
-                ret += static_cast<char>(16 * p1.value() + p2.value());
-                i += 3;
-            }
-            else {
-                return std::unexpected("invalid percent encoding url");
-            }
-        }
-        else {
-            ret += url[i];
-            ++i;
-        }
-    }
-
-    return ret;
-}
-
-std::map<std::string, std::string> parse_http_headers(std::string const& headers) {
-    std::map<std::string, std::string> m;
-
-    for (const auto line : views::split(headers, "\r\n"s)) {
-        auto str = std::ranges::to<std::string>(line);
+    std::vector<std::string> string_split(const std::string& str, const std::string& delimiter) const {
+        std::vector<std::string> tokens;
+        size_t start = 0;
+        size_t end = str.find(delimiter);
         
-        auto pos = str.find(": ");
-        auto key = str.substr(0, pos);
-        auto value = str.substr(pos + 2);
-
-        m.emplace(std::move(key), std::move(value));
-    }
-
-    std::cout << m.size() << "\n";
-    return m;
-}
-
-std::expected<Request, ParseError> parse_http_request(std::string const& data) {
-    Request req;
-    std::string headers;
-    ParseState state = ParseState::method;
-    const char* ptr = data.c_str();
-
-    while(*ptr != '\0') {
-        switch(state) {
-            case ParseState::method:
-                if (*ptr == ' ') {
-                    state = ParseState::url;
-                }
-                else {
-                    if (req.method.length() == METHOD_MAX_LEN) {
-                        return std::unexpected(ParseError::method_too_long);
-                    }
-
-                    req.method += *ptr;
-                }
-
-                ++ptr;
-                break;
-            case ParseState::url:
-                if (*ptr == ' ') {
-                    state = ParseState::version;
-                }
-                else {
-                    if (req.url.length() == URL_MAX_LEN) {
-                        return std::unexpected(ParseError::url_too_long);
-                    }
-
-                    req.url += *ptr;
-                }
-
-                ++ptr;
-                break;
-            case ParseState::version:
-                if (*ptr == '\r') {
-                    if (ptr[1] != '\n') {
-                        return std::unexpected(ParseError::invalid_crlf);
-                    }
-                    else {
-                        state = ParseState::headers;
-                        ptr += 2;
-                    }
-                }
-                else {
-                    if (req.version.length() == VERSION_MAX_LEN) {
-                        return std::unexpected(ParseError::version_too_long);
-                    }
-
-                    req.version += *ptr;
-                    ++ptr;
-                }
-
-                break;
-            case ParseState::headers:
-                if (*ptr == '\r') {
-                    if (ptr[1] != '\n') {
-                        return std::unexpected(ParseError::invalid_crlf);
-                    }
-
-                    if (ptr[2] == '\r' && ptr[3] == '\n') {
-                        state = ParseState::body;
-                        ptr += 4;
-                    }
-                    else {
-                        headers += "\r\n";
-                        ptr += 2;
-                    }
-                }
-                else {
-                    headers += *ptr;
-                    ++ptr;
-                }
-
-                break;
-            case ParseState::body:
-                req.body += *ptr;
-                ++ptr;
-                break;
-            default:
-                std::unreachable();
+        while (end != std::string::npos) {
+            tokens.push_back(str.substr(start, end - start));
+            start = end + delimiter.length();
+            end = str.find(delimiter, start);
         }
+        
+        tokens.push_back(str.substr(start));
+        return tokens;
     }
 
-    req.headers = std::move(parse_http_headers(headers));
-    return req;
+    bool split_headers(const std::string& str, Request& req) const {
+        auto tokens = string_split(str, CRLF);
+
+        for (const auto& token : tokens) {
+            auto iter = token.find(": ");
+
+            if (iter == std::string::npos) {   // invalid header.
+                return false;
+            }
+
+            std::string key = token.substr(0, iter);
+            std::string value = token.substr(iter + 2);
+
+            req.headers.emplace(key, value);
+        }
+
+        return true;
+    }
+public:
+    static constexpr int max_method_len = 32;
+    static constexpr int max_url_len = 1024;
+    static constexpr int max_version_len = 32;
+
+    Parser() {}
+
+    ParseResult parse(const std::string& data, size_t len, Request& req) const {
+        size_t i = 0;
+        State state = State::method;
+        std::string headers;
+        bool stop = false;
+
+        while (i < len && !stop) {
+            char c = data[i];
+
+            switch(state) {
+                case State::method:
+                    if (c == SPACE) {
+                        if (req.method.empty()) {
+                            return ParseResult::invalid_method;
+                        }
+
+                        state = State::url;
+                    }
+                    else if (c == CR || c == LF) {
+                        return ParseResult::invalid_format;
+                    }
+                    else {
+                        if (req.method.length() == max_method_len) {
+                            return ParseResult::method_too_long;
+                        }
+
+                        req.method += c;
+                    }
+
+                    ++i;
+                    break;
+                case State::url:
+                    if (c == SPACE) {
+                        if (req.url.empty()) {
+                            return ParseResult::invalid_url;
+                        }
+
+                        state = State::version;
+                    }
+                    else if (c == CR || c == LF) {
+                        return ParseResult::invalid_format;
+                    }
+                    else {
+                        if (req.url.length() == max_url_len) {
+                            return ParseResult::url_too_long;
+                        }
+
+                        req.url += c;
+                    }
+
+                    ++i;
+                    break;
+                case State::version:
+                    if (c == CR) {
+                        if (req.version.empty()) {
+                            return ParseResult::invalid_format;
+                        }
+
+                        if (i + 1 >= len) {
+                            return ParseResult::invalid_format;
+                        }
+                        else {
+                            if (data[i + 1] != LF) {
+                                return ParseResult::invalid_crlf;
+                            }
+                            else {
+                                state = State::headers;
+                                i += 2;
+                            }
+                        }
+                    }
+                    else {
+                        if (req.version.length() == max_version_len) {
+                            return ParseResult::version_too_long;
+                        }
+
+                        req.version += c;
+                        ++i;
+                    }
+
+                    break;
+                case State::headers:
+                    if (c == CR) {
+                        if (i + 1 >= len || data[i + 1] != LF) {
+                            return ParseResult::invalid_format;
+                        }
+                        else {
+                            if (i + 2 < len && data[i + 2] == CR && i + 3 < len && data[i + 3] == LF) {
+                                stop = true;
+                            }
+                            else {
+                                headers += CRLF;
+                                i += 2;
+                            }
+                        }
+                    }
+                    else if (c == LF) {   // this case means only 1 LF, no CR.
+                        return ParseResult::invalid_headers;
+                    }
+                    else {
+                        headers += c;
+                        ++i;
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (!split_headers(headers, req)) {
+            return ParseResult::invalid_headers;
+        }
+
+        return ParseResult::success;
+    }
+};
+
+const char* parse_result_str(ParseResult res) {
+    switch(res) {
+        case ParseResult::success:
+            return "success";
+        case ParseResult::method_too_long:
+            return "method too long";
+        case ParseResult::url_too_long:
+            return "url too long";
+        case ParseResult::version_too_long:
+            return "version too long";
+        case ParseResult::invalid_method:
+            return "invalid method";
+        case ParseResult::invalid_url:
+            return "invalid url";
+        case ParseResult::invalid_version:
+            return "invalid version";
+        case ParseResult::invalid_format:
+            return "invalid format";
+        case ParseResult::invalid_crlf:
+            return "invalid crlf";
+        case ParseResult::invalid_headers:
+            return "invalid headers";
+        default:
+            return "unknown parse error";
+    }
 }
 
 int main() {
-    std::string url = "GET http://www.hatsunemiku.com/ HTTP/1.1\r\n"
+    const std::string url = "GET http://www.hatsunemiku.com/ HTTP/1.1\r\n"
                             "Host: www.example.com\r\n"
                             "Content-Length: 10\r\n"
                             "Accept-Encoding: utf-8\r\n"
                             "\r\n"
                             "Hello World";
+    
+    Request req;
+    Parser parser;
+    ParseResult result = parser.parse(url, url.length(), req);
 
-    auto decodeUrl = percent_encoding_url_decode(url);
-    if (decodeUrl) {
-        auto req = parse_http_request(decodeUrl.value());
-        if (req) {
-            const auto& v = req.value();
-            std::cout << v.method << "\n";
-            std::cout << v.url << "\n";
-            std::cout << v.version << "\n";
-
-            for (const auto& [k, v] : v.headers) {
-                std::cout << k << " => " << v << "\n";
-            }
-
-            std::cout << v.body << "\n";
-        }
-        else {
-            std::cerr << get_parse_error_str(req.error()) << "\n";
-        }
+    if (result != ParseResult::success) {
+        std::cerr << parse_result_str(result) << "\n";
+        return 1;
     }
-    else {
-        std::cerr << decodeUrl.error() << "\n";
+
+    std::cout << req.method << "\n";
+    std::cout << req.url << "\n";
+    std::cout << req.version << "\n";
+
+    for (const auto&[k, v] : req.headers) {
+        std::cout << k << " " << v << "\n";
     }
+
+    return 0;
 }
